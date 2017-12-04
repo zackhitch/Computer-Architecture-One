9266bec1579eac5a2bcf48616b61c9081781a3af
Beej Jorgensen <beej@lambdaschool.com>
2017-12-04 00:07:24 -0800

    better str2code.js

--- ls8/str2code.js
+++ ls8/str2code.js
@@ -3,7 +3,13 @@
  * code for the simulated computer
  */
 
-str = 'Hello, world!\n';
+const argv = process.argv.slice(2);
+
+if (argv.length === 0) {
+    console.error('usage: str2code string');
+}
+
+const str = argv.join(' ');
 
 /**
  * Convert a value into a binary string of at least 8 bits
@@ -23,6 +29,19 @@ for (let i = 0; i < str.length; i++) {
     const val = str.charCodeAt(i);
     const binVal = makeBinString(val);
 
-    const ascii = val >= 32 && val <= 126? str[i]: '??';
+    let ascii
+
+    if (val == 10) {
+        ascii = '[newline]';
+    } else if (val == 13) {
+        ascii = '[carriage return]';
+    } else if (val == 32) {
+        ascii = '[space]';
+    } else if (val < 32 || val > 126) {
+        ascii = '[??]';
+    } else {
+        ascii = str[i];
+    }
+    
     console.log(`${binVal} # ${ascii}`);
 }
