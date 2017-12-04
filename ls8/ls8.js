8a616e0800a05de8233b5d6f313dd5f110f4b796
Beej Jorgensen <beej@lambdaschool.com>
2017-12-03 22:01:13 -0800

    add ability to read from a file

--- ls8/ls8.js
+++ ls8/ls8.js
@@ -2,62 +2,73 @@ const fs = require('fs');
 const CPU = require('./cpu');
 
 /**
- * Load the file into the CPU from stdin
+ * Process a loaded file
+ */
+function processFile(content, cpu, onComplete) {
+    // Pointer to the memory address in the CPU that we're
+    // loading a value into:
+    let curAddr = 0;
+    const lines = content.split('\n');
+
+    // Loop through each line of machine code
+
+    for (let line of lines) {
+        // Hunt for a comment
+        const commentIndex = line.indexOf('#');
+
+        if (commentIndex != -1) {
+            line = line.substr(0, commentIndex);
+        }
+
+        // Remove whitespace from either end
+        line = line.trim();
+
+        if (line === '') {
+            // Line was blank or only a comment
+            continue;
+        }
+
+        // At this point, the line should just be the 1s and 0s
+
+        // Convert from binary string to number
+        const binValue = parseInt(line, 2); // Base 2 == binary
+
+        // Check to see if the parsing failed
+        if (isNaN(binValue)) {
+            console.error('Invalid binary number: ' + line);
+            process.exit(1);
+        }
+
+        // Ok, we have a good value, so store it into memory:
+        //console.log(`storing ${binValue}, ${line}`);
+        cpu.poke(curAddr, binValue);
+
+        // And on to the next one
+        curAddr++;
+    }
+
+    onComplete(cpu);
+}
+
+/**
+ * Load the instructions into the CPU from stdin
  */
 function loadFileFromStdin(cpu, onComplete) {
     let content = '';
-
-    function processFile() {
-        // Pointer to the memory address in the CPU that we're
-        // loading a value into:
-        let curAddr = 0;
-        const lines = content.split('\n');
-
-        // Loop through each line of machine code
-
-        for (let line of lines) {
-            // Hunt for a comment
-            const commentIndex = line.indexOf('#');
-
-            if (commentIndex != -1) {
-                line = line.substr(0, commentIndex);
-            }
-
-            // Remove whitespace from either end
-            line = line.trim();
-
-            if (line === '') {
-                // Line was blank or only a comment
-                continue;
-            }
-
-            // At this point, the line should just be the 1s and 0s
-
-            // Convert from binary string to number
-            const binValue = parseInt(line, 2); // Base 2 == binary
-
-            // Check to see if the parsing failed
-            if (isNaN(binValue)) {
-                console.error('Invalid binary number: ' + line);
-                process.exit(1);
-            }
-
-            // Ok, we have a good value, so store it into memory:
-            //console.log(`storing ${binValue}, ${line}`);
-            cpu.poke(curAddr, binValue);
-
-            // And on to the next one
-            curAddr++;
-        }
-
-        onComplete(cpu);
-    }
 
     // Read everything from standard input, stolen from:
     // https://stackoverflow.com/questions/13410960/how-to-read-an-entire-text-stream-in-node-js
     process.stdin.resume();
     process.stdin.on('data', function(buf) { content += buf.toString(); });
-    process.stdin.on('end', processFile);
+    process.stdin.on('end', () => { processFile(content, cpu, onComplete); });
+}
+
+/**
+ * Load the instructions into the CPU from a file
+ */
+function loadFile(filename, cpu, onComplete) {
+    const content = fs.readFileSync(filename, 'utf-8');
+    processFile(content, cpu, onComplete);
 }
 
 /**
@@ -74,4 +85,19 @@ function onFileLoaded(cpu) {
  */
 
 let cpu = new CPU();
-loadFileFromStdin(cpu, onFileLoaded);
+
+// Get remaining command line arguments
+const argv = process.argv.slice(2);
+
+// Check arguments
+if (argv.length === 0) {
+    // Read from stdin
+    loadFileFromStdin(cpu, onFileLoaded);
+} else if (argv.length == 1) {
+    // Read from file
+    loadFile(argv[0], cpu, onFileLoaded);
+} else {
+    console.error('usage: ls8 [machinecodefile]');
+    process.exit(1);
+
+}
