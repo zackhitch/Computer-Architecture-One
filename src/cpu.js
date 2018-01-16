330f882183135a358a7d5596ef2e1fd0ecba2adf
Brian "Beej Jorgensen" Hall <beej@beej.us>
2018-01-15 19:24:08 -0800

    add skeleton:

---
+++ src/cpu.js
@@ -0,0 +1,141 @@
+/**
+ * LS-8 v2.0 emulator skeleton code
+ */
+
+const fs = require('fs');
+
+// Instructions
+
+const HLT  = 0b00011011; // Halt CPU
+// !!! IMPLEMENT ME
+// LDI
+// MUL
+// PRN
+
+/**
+ * Class for simulating a simple Computer (CPU & memory)
+ */
+class CPU {
+
+    /**
+     * Initialize the CPU
+     */
+    constructor(ram) {
+        this.ram = ram;
+
+        this.reg = new Array(8).fill(0); // General-purpose registers
+        
+        // Special-purpose registers
+        this.reg.PC = 0; // Program Counter
+        this.reg.IR = 0; // Instruction Register
+
+		this.setupBranchTable();
+    }
+	
+	/**
+	 * Sets up the branch table
+	 */
+	setupBranchTable() {
+		let bt = {};
+
+        bt[HLT] = this.HLT;
+        // !!! IMPLEMENT ME
+        // LDI
+        // MUL
+        // PRN
+
+		this.branchTable = bt;
+	}
+
+    /**
+     * Store value in memory address, useful for program loading
+     */
+    poke(address, value) {
+        this.ram.write(address, value);
+    }
+
+    /**
+     * Starts the clock ticking on the CPU
+     */
+    startClock() {
+        const _this = this;
+
+        this.clock = setInterval(() => {
+            _this.tick();
+        }, 1);
+    }
+
+    /**
+     * Stops the clock
+     */
+    stopClock() {
+        clearInterval(this.clock);
+    }
+
+    /**
+     * ALU functionality
+     * 
+     * op can be: ADD SUB MUL DIV INC DEC CMP
+     */
+    alu(op, regA, regB) {
+        switch (op) {
+            case 'MUL':
+                // !!! IMPLEMENT ME
+                break;
+        }
+    }
+
+    /**
+     * Advances the CPU one cycle
+     */
+    tick() {
+        // !!! IMPLEMENT ME
+
+        // Load the instruction register from the current PC
+
+        // Debugging output
+        //console.log(`${this.reg.PC}: ${this.reg.IR.toString(2)}`);
+
+        // Based on the value in the Instruction Register, jump to the
+        // appropriate hander in the branchTable
+
+        // Check that the handler is defined, halt if not (invalid
+        // instruction)
+
+		// We need to use call() so we can set the "this" value inside
+		// the handler (otherwise it will be undefined in the handler)
+		handler.call(this);
+    }
+
+    // INSTRUCTION HANDLER CODE:
+
+    /**
+     * HLT
+     */
+    HLT() {
+        // !!! IMPLEMENT ME
+    }
+
+    /**
+     * LDI R,I
+     */
+    LDI() {
+        // !!! IMPLEMENT ME
+    }
+
+    /**
+     * MUL R,R
+     */
+    MUL() {
+        // !!! IMPLEMENT ME
+    }
+
+    /**
+     * PRN R
+     */
+    PRN() {
+        // !!! IMPLEMENT ME
+    }
+}
+
+module.exports = CPU;
